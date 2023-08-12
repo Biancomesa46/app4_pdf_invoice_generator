34a7d847e686efa4b748a9364d3d9056cdfc1043
Niko <zoikasz.nikolasz@gmail.com>
2023-08-12 15:12:42 +0200

    Creating the basics of the pdf files

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 import glob
-
+from fpdf import FPDF
 import pandas
+
+
 
 filepaths = glob.glob("invoices/*.xlsx")
 print(filepaths)
 for filepath in filepaths:
     df = pandas.read_excel(filepath, sheet_name="Sheet 1")
+    pdf = FPDF(orientation="p", unit="mm", format="a4")
+    pdf.set_auto_page_break(auto=False, margin=0)
+    pdf.add_page()
+    pdf.set_text_color(100, 100, 100)
+    pdf.set_font(family="Times", style="B", size=12)
+    pdf.cell(txt=f"Invoice nr.{filepath[9:14]}", border=0, h=10, w=0, ln=1, align='l')
+    pdf.cell(txt=f"Date {filepath[15:24]}", h=10, w=0, ln=1, border=0, align='l')
+    pdf.output("PDFs/" + filepath.strip('invoices\\.xlsx') + ".pdf")
